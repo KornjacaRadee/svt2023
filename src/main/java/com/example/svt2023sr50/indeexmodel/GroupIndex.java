79fd9103af72ce1f9cf4c1e84cf495075b56c7a7
KornjacaRadee <djuricamilorad11@gmail.com>
2024-06-27 11:36:12 +0200

    Created analyzer

--- src/main/java/com/example/svt2023sr50/indeexmodel/GroupIndex.java
+++ src/main/java/com/example/svt2023sr50/indeexmodel/GroupIndex.java
@@ -6,21 +6,24 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Setting;
 
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @Document(indexName = "groups")
+@Setting(settingPath = "/configuration/serbian-analyzer-config.json")
 public class GroupIndex {
 
     @Id
+
     private Long id;
 
-    @Field(type = FieldType.Text)
+    @Field(type = FieldType.Text, store = true, name = "name", analyzer = "serbian_simple", searchAnalyzer = "serbian_simple")
     private String name;
 
-    @Field(type = FieldType.Text)
+    @Field(type = FieldType.Text, store = true, name = "description", analyzer = "serbian_simple", searchAnalyzer = "serbian_simple")
     private String description;
 
     @Field(type = FieldType.Date)
